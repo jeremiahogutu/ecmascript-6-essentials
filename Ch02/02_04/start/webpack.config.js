727ab37a30bd6c7f49929af37c55119fe9f76229
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2017-08-16 14:29:28 -0400

    transpiling with webpack

--- Ch02/02_04/start/webpack.config.js
+++ Ch02/02_04/start/webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
 	entry: './script.js',
 	output: {filename: 'bundle.js'},
 	module: {
-		loader: [
-			{test: /\.js?/, loader: 'babel-loader' exclude: /node_modules/}
+		loaders: [
+			{test: /\.js?/, loader: 'babel-loader', exclude: /node_modules/}
 		]
 	}
 };
